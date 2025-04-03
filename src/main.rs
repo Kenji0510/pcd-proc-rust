55c715c1915b3eaec9d00be0a306d1a79ae4957e
Kenji0510 <yukkurireimupikacyuu@gmail.com>
2025-04-03 15:51:00 +0900

    2025/04/03

--- src/main.rs
+++ src/main.rs
@@ -37,10 +37,8 @@ fn hsv_to_rgb(h: f32, s: f32, v: f32) -> [u8; 3] {
     ]
 }
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let reader = match Reader::open(
-        "/home/kenji/workspace/Rust/pcd-sample/data/Laser_map/voxelization/Laser_map_45.pcd",
-    ) {
+fn load_pcd(path: &str) -> Result<Vec<Point>, Box<dyn std::error::Error>> {
+    let reader = match Reader::open(path) {
         Ok(reader) => reader,
         Err(e) => {
             eprintln!("Error opening file: {}", e);
@@ -55,18 +53,49 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             return Err(e.into());
         }
     };
-    // println!("There are {} points", points.len());
-    // println!("points: {:?}", points[0]);
+    println!("There are {} points", points.len());
 
-    let point_cloud: Vec<(f32, f32, f32)> = points.iter().map(|pt| (pt.x, pt.y, pt.z)).collect();
+    Ok(points)
+}
 
-    let min_z = points.iter().map(|pt| pt.z).fold(f32::INFINITY, f32::min);
-    let max_z = points
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let pcd_path01 =
+        "/home/kenji/workspace/Rust/pcd-sample/data/Laser_map/voxelization/Laser_map_45.pcd";
+
+    let pcd_path02 = "/home/kenji/workspace/Rust/pcd-sample/data/cr/voxelization/cr_45.pcd";
+
+    let points_01 = match load_pcd(pcd_path01) {
+        Ok(points) => points,
+        Err(e) => {
+            eprintln!("Error loading PCD file: {}", e);
+            return Err(e.into());
+        }
+    };
+
+    let points_02 = match load_pcd(pcd_path02) {
+        Ok(points) => points,
+        Err(e) => {
+            eprintln!("Error loading PCD file: {}", e);
+            return Err(e.into());
+        }
+    };
+
+    let point_cloud01: Vec<(f32, f32, f32)> =
+        points_01.iter().map(|pt| (pt.x, pt.y, pt.z)).collect();
+
+    let point_cloud02: Vec<(f32, f32, f32)> =
+        points_02.iter().map(|pt| (pt.x, pt.y, pt.z)).collect();
+
+    let min_z = points_01
+        .iter()
+        .map(|pt| pt.z)
+        .fold(f32::INFINITY, f32::min);
+    let max_z = points_01
         .iter()
         .map(|pt| pt.z)
         .fold(f32::NEG_INFINITY, f32::max);
 
-    let point_cloud_color: Vec<[u8; 3]> = points
+    let point_cloud_color: Vec<[u8; 3]> = points_01
         .iter()
         .map(|pt| {
             // z座標を [0, 1] に正規化。minとmaxが同じ場合は0.0とする
@@ -82,17 +111,23 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         })
         .collect();
 
-    let positions = Points3D::new(point_cloud.into_iter());
+    let positions01 = Points3D::new(point_cloud01.into_iter());
+    let positions02 = Points3D::new(point_cloud02.into_iter());
 
     let rec = RecordingStreamBuilder::new("rerun_example_pcd").spawn()?;
 
     rec.log(
         "pcd",
-        &positions.with_colors(
+        &positions01.with_colors(
             point_cloud_color
                 .into_iter()
                 .map(|color| Color::from_rgb(color[0], color[1], color[2])),
         ),
+    )?;
+
+    rec.log(
+        "pcd02",
+        &positions02.with_colors([Color::from_rgb(255, 0, 0)]),
     )?;
 
     Ok(())
